b66b020ecb55c3d80ff4ca6dffd1bd27bbb4c194
bar haim <barha@il.ibm.com>
2022-10-02 22:08:08 +0300

    readme

---
+++ README.md
@@ -0,0 +1,20 @@
+# ISS 3D Tracker (03 Better Earth)
+
+## Running the app
+
+To run the app, you need to have Python3.7+ installed. Then, install the dependencies:
+
+```bash
+pip install -r requirements.txt
+```
+
+Then, run the app:
+
+```bash
+cd backend
+python server.py
+```
+
+Now, open your browser at http://localhost:4000
+
+Enjoy!
--- backend/server.py
+++ backend/server.py
@@ -110,5 +110,5 @@ app.mount("/", StaticFiles(directory="static", html=True), name="static")
 
 if __name__ == '__main__':
     logging.getLogger().setLevel(logging.INFO)
-    uvicorn.run('server:app', host='0.0.0.0', port=80, reload=True, limit_max_requests=1000)
+    uvicorn.run('server:app', host='0.0.0.0', port=4000, reload=True, limit_max_requests=1000)
 
